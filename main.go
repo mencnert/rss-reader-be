ef9b9c5b8050c55d9f1a70ae3633c7c1fd8e3a38
tomas.mencner <mencnert@gmail.com>
2021-11-24 12:26:37 +0100

    fix typo

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	if err := cmd.Execute("PORT", "LOGIN", "PASSWORD", "RSS_FETCH_EVERY_N_SECS", "DATABASE_URL"); err {
+	if err := cmd.Execute("PORT", "LOGIN", "PASSWORD", "RSS_FETCH_EVERY_N_SECS", "DATABASE_URL"); err != nil {
 		log.Fatalf("Unable to start commands: %v", err)
 	}
 }
